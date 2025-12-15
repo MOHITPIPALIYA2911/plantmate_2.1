98697adc0936398e0272585ea1c401bd75b3b6ae
Mohit Pipaliya <pipaliyamohit4g@gmail.com>
2025-12-15 06:17:56 +0530

    Notification bkend in prog

--- server/cron.js
+++ server/cron.js
@@ -1,7 +1,7 @@
 const cron = require("node-cron");
 const { runCareScheduler } = require("./services/careScheduler");
 
-// Run daily at 7 AM
+// Run daily at 7 AM.
 cron.schedule("0 7 * * *", async () => {
   console.log("⏰ Daily Care Scheduler Running...");
   await runCareScheduler();
